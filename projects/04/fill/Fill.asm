cf33bb531d8719495991e4262b2515fcb7a27ca3
Sam Davis <sam@samgd.com>
2020-01-11 15:05:43 +0000

    Add Fill HDL implementation

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,52 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    @KBD
+    D=M
+
+    @fill
+    M=0
+
+    @START
+    D;JEQ
+
+    // if MEMORY[KBD] != 0: fill = -1
+    @fill
+    M=-1
+
+(START)
+    @SCREEN
+    D=A
+    @i
+    M=D
+
+(FILL)
+    @i
+    D=M
+    @SCREEN
+    D=D-A
+    @8192
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // load value and fill
+    @fill
+    D=M
+
+    @i
+    A=M
+    M=D
+
+    @i
+    M=M+1
+
+    @FILL
+    0;JMP
+
